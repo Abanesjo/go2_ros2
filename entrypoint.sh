2bafd20223862f956d97e1d48f4273f3e2b566c3
John Abanes <john.abanes@nyu.edu>
2026-04-19 21:25:55 -0400

    volume mount cyclonedds.xml for faster build

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ source /workspace/ros2_ws/install/setup.bash
 
 echo "source /workspace/ros2_ws/install/setup.bash" >> ~/.bashrc
 echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> ~/.bashrc
-echo "export CYCLONEDDS_URI=file:///workspace/dependencies/cyclonedds.xml" >> ~/.bashrc
+echo "export CYCLONEDDS_URI=file:///workspace/ros2_ws/src/cyclonedds.xml" >> ~/.bashrc
 echo "alias mujoco='export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/x86_64-linux-gnu'" >> ~/.bashrc
 echo "alias cleanshell='env -i HOME=$HOME PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin bash --norc --noprofile'" >> ~/.bashrc
 
